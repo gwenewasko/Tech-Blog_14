d7c77e877071d2a57ee9c6dfcac0bea5376cfeb7
Gwen Ewasko <gwenewasko@gmail.com>
2022-04-02 08:58:53 -0500

    adding comment section

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,12 @@
 const router = require("express").Router();
-const { Post, User } = require("../models");
+const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
     // Get all posts and JOIN with user data
     const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
+      include: [User, Comment],
     });
 
     // Serialize data so the template can read it
